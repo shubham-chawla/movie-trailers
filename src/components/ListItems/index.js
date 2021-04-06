d787d413b32a66ee531e6284ffb9af9cbbe6447a
shubham-chawla <chawlashubham007@gmail.com>
2021-04-07 03:19:30 +0530

    chores

--- src/components/ListItems/index.js
+++ src/components/ListItems/index.js
@@ -5,21 +5,19 @@ import { FaRegPlayCircle } from 'react-icons/fa';
 import YoutubePlayer from '../YoutubePlayer';
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
+    display: grid;
+    grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
+    -ms-grid-columns: (1fr)[${({ columns }) => columns}];
+    grid-gap: 30px;
+    padding: 20px;
     background-color: #4a4a4a;
     color: #ffffff;
+    transition: all 0.1s cubic-bezier(0.25, 0.46, 0.45, 0.94);
 `;
 
 const Item = styled.div`
     display: flex;
     flex-direction: column;
-    width: 232px;
-    min-height: 300px;
-    margin: 40px auto;
-    padding: 0 10px;
     position: relative;
 `;
 
@@ -27,8 +25,8 @@ const CoverLayout = styled.div`
     background: linear-gradient(to top, rgba(0, 0, 0, 0.7) 0%, transparent 50%);
     background-size: cover;
     position: absolute;
-    height: 300px;
-    width: 212px;
+    height: 100%;
+    width: 100%;
 `;
 
 const StyledImage = styled.img`
@@ -42,13 +40,13 @@ const ListItems = ({ className, data }) => {
     const [vId, setVId] = useState(null);
     const [obj, currObj] = useState(null);
     const [itemIdx, setIdx] = useState(null);
-    const [items, setItems] = useState(null);
+    const [items, setItems] = useState(5);
 
     useEffect(() => {
         const calculateWidth = () => {
             const width = Math.abs(window.innerWidth - 100);
-            const calculatedItems = Math.floor(width / 232);
-            setItems(calculatedItems > 5 ? 5 : calculatedItems);
+            const calculatedItems = Math.floor(width / 250);
+            setItems(calculatedItems);
         };
 
         calculateWidth();
@@ -63,14 +61,14 @@ const ListItems = ({ className, data }) => {
     const getVId = data => data.TrailerURL.split('?v=')[1];
 
     return (
-        <Container className={`ListItems ${className || ''}`}>
+        <Container columns={items} className={`ListItems ${className || ''}`}>
             {Object.keys(data).map((x, i) => (
                 <React.Fragment key={i}>
-                    {vId && i === itemIdx && <YoutubePlayer vId={vId} obj={obj} bgImg={`https://in.bmscdn.com/events/moviecard/${obj.EventImageCode}.jpg`} />}
+                    {vId && i === itemIdx && <YoutubePlayer columns={items} vId={vId} obj={obj} bgImg={`https://in.bmscdn.com/events/moviecard/${obj.EventImageCode}.jpg`} />}
                     <Item key={i}>
                         <StyledImage border={vId === getVId(data[x])} src={`https://in.bmscdn.com/events/moviecard/${data[x].EventImageCode}.jpg`} alt={data[x].EventImageCode} />
                         {vId !== getVId(data[x]) && <CoverLayout />}
-                        <div className="small white margin-t-5">{data[x].EventName}</div>
+                        <div className="small white margin-t-5 zIndex">{data[x].EventName}</div>
                         {vId !== getVId(data[x]) && (
                             <FaRegPlayCircle
                                 className="play-btn white"
